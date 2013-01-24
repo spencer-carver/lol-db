c7faf94114e524d0915eb459a4065074eddc5d5b
SparkSovereign <groppcw@rose-hulman.edu>
2013-01-24 09:42:33 -0800

    Create Active.java

---
+++ LOLdb/src/lol/Active.java
@@ -0,0 +1,11 @@
+package lol;
+
+public class Active
+{
+  private int cID;
+  private String Name;
+  private double Dmg;
+  private double Dmg_plus;
+  private double CD;
+  private double CD_plus;
+}
