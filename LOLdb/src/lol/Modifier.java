a9d86f5cbbf172449b5552302dc66a132fd30c5a
SparkSovereign <groppcw@rose-hulman.edu>
2013-01-24 09:37:44 -0800

    Create Modifier.java

---
+++ LOLdb/src/lol/Modifier.java
@@ -0,0 +1,16 @@
+package lol;
+
+public class Modifier
+{
+  private int mID;
+  private String name;
+  private double HP;
+  private double HP5;
+  private double MP;
+  private double MP5;
+  private double Dmg;
+  private double ASpd;
+  private double MR;
+  private double MS;
+  private double AP;
+}
