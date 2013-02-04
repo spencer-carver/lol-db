efea8933fabe508fe63b39496f5dab499019e1cc
SparkSovereign <groppcw@rose-hulman.edu>
2013-02-04 12:56:13 -0500

    Update LOLdb/src/lol/Modifier.java

--- LOLdb/src/lol/Modifier.java
+++ LOLdb/src/lol/Modifier.java
@@ -1,26 +1,30 @@
 package lol;
 
-public class Modifier
+public abstract class Modifier
 {
-  private int mID;
-  private String name;
-  private double health;
-  private double healthPerLevel;
-	private double healthRegen;
-	private double healthRegenPerLevel;
-	private double resourceType;
-	private double mana;
-	private double manaPerLevel;
-	private double manaRegen;
-	private double manaRegenPerLevel;
-	private double damage;
-	private double damagePerLevel;
-	private double attackSpeed;
-	private double attackSpeedPerLevel;
-	private double armor;
-	private double armorPerLevel;
-	private double magicResist;
-	private double magicResistPerLevel;
-	private double moveSpeed;
-	private double attackRange;
+	private int mID;
+	private String name;
+	private char guid;
+	private double health; 			public double getHealth() { return health; }
+	private double healthPerLevel; 		public double getHealthPerLevel() { return healthPerLevel; }
+	private double healthRegen;		public double getHealthRegen() { return healthRegen; }
+	private double healthRegenPerLevel;	public double getHealthRegenPerLevel() { return healthRegenPerLevel; }
+	private double mana;			public double getMana() { return mana; }
+	private double manaPerLevel;		public double getManaPerLevel() { return manaPerLevel; }
+	private double manaRegen;		public double getManaRegen() { return manaRegen; }
+	private double manaRegenPerLevel;	public double getManaRegenPerLevel() { return manaRegenPerLevel; }
+	private double damage;			public double getDamage() { return damage; }
+	private double damagePerLevel;		public double getDamagePerLevel() { return damagePerLevel; }
+	private double attackSpeed;		public double getAttackSpeed() { return attackSpeed; }
+	private double attackSpeedPerLevel;	public double getAttackSpeedPerLevel() { return attackSpeedPerLevel; }
+	private double armor;			public double getArmor() { return armor; }
+	private double armorPerLevel;		public double getArmorPerLevel() { return armorPerLevel; }
+	private double magicResist;		public double getMagicResist() { return magicResist; }
+	private double magicResistPerLevel;	public double getMagicResistPerLevel() { return magicResistPerLevel; }
+	private double moveSpeed;		public double getMoveSpeed() { return moveSpeed; }
+	
+	public char getGuid()
+	{
+		return guid;
+	}
 }
