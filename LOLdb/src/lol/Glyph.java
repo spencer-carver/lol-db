79827e9e7ae7c911dc0bf8033f0a678ceebe3986
SparkSovereign <groppcw@rose-hulman.edu>
2013-02-04 13:06:49 -0500

    Update LOLdb/src/lol/Glyph.java

--- LOLdb/src/lol/Glyph.java
+++ LOLdb/src/lol/Glyph.java
@@ -2,5 +2,10 @@ package lol;
 
 public class Glyph extends Rune
 {
-
+  private static ArrayList<Glyph> glyphs = new ArrayList<Glyph>();
+  public Glyph()
+  {
+    super();
+    glyphs.add(this);
+  }
 }
