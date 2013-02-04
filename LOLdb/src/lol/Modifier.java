e42b5c294b218e021e9506168be8d17ca03346a2
SparkSovereign <groppcw@rose-hulman.edu>
2013-02-04 12:33:55 -0500

    Update LOLdb/src/lol/Modifier.java

--- LOLdb/src/lol/Modifier.java
+++ LOLdb/src/lol/Modifier.java
@@ -4,13 +4,23 @@ public class Modifier
 {
   private int mID;
   private String name;
-  private double HP;
-  private double HP5;
-  private double MP;
-  private double MP5;
-  private double Dmg;
-  private double ASpd;
-  private double MR;
-  private double MS;
-  private double AP;
+  private double health;
+  private double healthPerLevel;
+	private double healthRegen;
+	private double healthRegenPerLevel;
+	private double resourceType;
+	private double mana;
+	private double manaPerLevel;
+	private double manaRegen;
+	private double manaRegenPerLevel;
+	private double damage;
+	private double damagePerLevel;
+	private double attackSpeed;
+	private double attackSpeedPerLevel;
+	private double armor;
+	private double armorPerLevel;
+	private double magicResist;
+	private double magicResistPerLevel;
+	private double moveSpeed;
+	private double attackRange;
 }
