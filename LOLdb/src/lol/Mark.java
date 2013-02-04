83b8f077175973c29ab09d27b7bea11974437f54
SparkSovereign <groppcw@rose-hulman.edu>
2013-02-04 13:14:09 -0500

    Update LOLdb/src/lol/Mark.java

--- LOLdb/src/lol/Mark.java
+++ LOLdb/src/lol/Mark.java
@@ -1,6 +1,13 @@
 package lol;
 
+import java.util.ArrayList;
+
 public class Mark extends Rune
 {
-
+  public static ArrayList<Mark> marks = new ArrayList<Mark>();
+  public Mark()
+  {
+    super();
+    marks.add(this);
+  }
 }
