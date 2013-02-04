a59d517478803624a5911c36b434365a70fe7f71
SparkSovereign <groppcw@rose-hulman.edu>
2013-02-04 13:25:10 -0500

    Update LOLdb/src/lol/Modifier.java

--- LOLdb/src/lol/Modifier.java
+++ LOLdb/src/lol/Modifier.java
@@ -9,21 +9,39 @@ public abstract class Modifier
 	private double health; 			public double getHealth() { return health; }
 		public void setHealth(double h) { health = h; }
 	private double healthPerLevel; 		public double getHealthPerLevel() { return healthPerLevel; }
+		public void setHealthPerLevel(double h) { healthPerLevel = h; }
 	private double healthRegen;		public double getHealthRegen() { return healthRegen; }
+		public void setHealthRegen(double h) { healthRegen = h; }
 	private double healthRegenPerLevel;	public double getHealthRegenPerLevel() { return healthRegenPerLevel; }
+		public void setHealthRegenPerLevel(double h) { healthRegenPerLevel = h; }
 	private double mana;			public double getMana() { return mana; }
+		public void setMana(double m) { mana = m; }
 	private double manaPerLevel;		public double getManaPerLevel() { return manaPerLevel; }
+		public void setManaPerLevel(double m) { manaPerLevel = m; }
 	private double manaRegen;		public double getManaRegen() { return manaRegen; }
+		public void setManaRegen(double m) { manaRegen = m; }
 	private double manaRegenPerLevel;	public double getManaRegenPerLevel() { return manaRegenPerLevel; }
+		public void setManaRegenPerLevel(double m) { manaRegenPerLevel = m; }
 	private double damage;			public double getDamage() { return damage; }
+		public void setDamage(double d) { damage = d; }
 	private double damagePerLevel;		public double getDamagePerLevel() { return damagePerLevel; }
+		public void setDamagePerLevel(double d) { damagePerLevel = d; }
 	private double attackSpeed;		public double getAttackSpeed() { return attackSpeed; }
+		public void setAttackSpeed(double as) { attackSpeed = as; }
 	private double attackSpeedPerLevel;	public double getAttackSpeedPerLevel() { return attackSpeedPerLevel; }
+		public void setAttackSpeedPerLevel(double as) { attackSpeedPerLevel = as; }
 	private double armor;			public double getArmor() { return armor; }
+		public void setArmor(double a) { armor = a; }
 	private double armorPerLevel;		public double getArmorPerLevel() { return armorPerLevel; }
+		public void setArmorPerLevel(double a) { armorPerLevel = a; }
 	private double magicResist;		public double getMagicResist() { return magicResist; }
+		public void setMagicResist(double mr) { magicResist = mr; }
 	private double magicResistPerLevel;	public double getMagicResistPerLevel() { return magicResistPerLevel; }
+		public void setMagicResistPerLevel(double mr) { magicResistPerLevel = mr; }
 	private double moveSpeed;		public double getMoveSpeed() { return moveSpeed; }
+		public void setMoveSpeed(double ms) { moveSpeed = ms; }
+	private double moveSpeedMultiplier;	public double getMoveSpeedMultiplier() { return moveSpeedMultiplier; }
+		public void setMoveSpeedMultiplier(double ms) { moveSpeedMultipler = ms; }
 	
 	public Modifier()
 	{
@@ -47,5 +65,6 @@ public abstract class Modifier
 		magicResist = 0;
 		magicResistPerLevel = 0;
 		moveSpeed = 0;
+		moveSpeedMultiplier = 1;
 	}
 }
